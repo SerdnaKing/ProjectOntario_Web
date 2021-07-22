1825abf6fbb61d422f65125703bae25218ddaf5c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2021-07-22 15:43:13 -0400

    setup pondside sandbag controls and prefab

--- Assets/Scripts/SandbagControls.cs
+++ Assets/Scripts/SandbagControls.cs
@@ -8,19 +8,22 @@ public class SandbagControls : MonoBehaviour
     public GameObject lakeSideSandbags;
     public Toggle lakeSideSandbagsToggle;
 
+    public GameObject pondSideSandbags;
+    public Toggle pondSideSandbagsToggle;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKeyDown(KeyCode.L) && lakeSideSandbags)
         {
             ToggleLakesideSandbags(!lakeSideSandbags.activeSelf);
-            lakeSideSandbagsToggle.isOn = lakeSideSandbags.activeSelf;
+            if(lakeSideSandbagsToggle) lakeSideSandbagsToggle.isOn = lakeSideSandbags.activeSelf;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && pondSideSandbags)
+        {
+            TogglePondsideSandbags(!pondSideSandbags.activeSelf);
+            if (pondSideSandbagsToggle) pondSideSandbagsToggle.isOn = pondSideSandbags.activeSelf;
         }
     }
 
@@ -28,7 +31,7 @@ public class SandbagControls : MonoBehaviour
     /// Will enable/disable the lakeside sandbag parent and every sandbag too.
     /// We may allow for user to disable some lakeside sandbags so this toggle can be used to re-enable them.
     /// 
-    /// If this is seen as a bug by users, we can remove the child gameobject enabliling/disabling.
+    /// If this is seen as a bug by users, we can remove the child gameobject enabling/disabling.
     /// </summary>
     /// <param name="selected">Toggle's state</param>
     public void ToggleLakesideSandbags(bool selected)
@@ -37,6 +40,22 @@ public class SandbagControls : MonoBehaviour
         for (int i = 0; i < lakeSideSandbags.transform.childCount; i++)
         {
             lakeSideSandbags.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// Will enable/disable the pondside sandbag parent and every sandbag too.
+    /// We may allow for user to disable some pondside sandbags so this toggle can be used to re-enable them.
+    /// 
+    /// If this is seen as a bug by users, we can remove the child gameobject enabling/disabling.
+    /// </summary>
+    /// <param name="selected">Toggle's state</param>
+    public void TogglePondsideSandbags(bool selected)
+    {
+        pondSideSandbags.SetActive(selected);
+        for (int i = 0; i < pondSideSandbags.transform.childCount; i++)
+        {
+            pondSideSandbags.transform.GetChild(i).gameObject.SetActive(true);
         }
     }
 }
