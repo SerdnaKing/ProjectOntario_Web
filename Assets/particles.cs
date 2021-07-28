77fdd5ce2d4c9475f86ecc3684b25f32dc52e390
Chase Call <ccc9775@rit.edu>
2021-07-28 14:36:33 -0400

    added to sandbag and particles for cars
    added to the prefab for the sandbags so that they mask water in an area behind them, and added a base particle system that can be applied to the cars and propane tanks in the main scene

---
+++ Assets/particles.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particles : MonoBehaviour
+{
+    public double height;
+    public GameObject water;
+    ParticleSystem smoke;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smoke = GetComponent<ParticleSystem>();  
+    }
+
+    void Update()
+    {
+        if(water.transform.position.y >= height-11.4)
+        {
+            Debug.Log("particle should play now");
+            smoke.Play();
+        }
+        else
+        {
+            smoke.Stop();
+        }
+    }
+}
